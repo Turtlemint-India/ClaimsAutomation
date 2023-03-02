92b523746d7f12149d5a99b0509f10477f0965b0
tejasbahadure <tejas.bahadure@turtlemint.com>
2023-03-02 17:10:47 +0530

    code updated for upload

--- src/test/java/ClaimsAmbrish/DocumentUploadAndAdditionalInfoTest.java
+++ src/test/java/ClaimsAmbrish/DocumentUploadAndAdditionalInfoTest.java
@@ -3,7 +3,11 @@ package ClaimsAmbrish;
 import com.qa.turtlemint.base.TestBase;
 import com.qa.turtlemint.pages.ClaimsAmbrish.AdditionalInfoPage;
 import com.qa.turtlemint.pages.ClaimsAmbrish.DocumentUploadPage;
+import com.qa.turtlemint.pages.MedWorkINIT.ClaimsHomePG;
+import com.qa.turtlemint.pages.MedWorkINIT.Initiimate;
+import com.qa.turtlemint.pages.MedWorkINIT.Medical;
 import com.qa.turtlemint.pages.login.LoginPage;
+import com.qa.turtlemint.pages.mis.MIS;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -19,21 +23,40 @@ public class DocumentUploadAndAdditionalInfoTest extends TestBase {
     DocumentUploadPage dup;
     AdditionalInfoPage aip;
 
+    ClaimsHomePG ClaimHm;
+    Medical Med;
+    MIS mis;
+    public String misPolicy;
+
     public DocumentUploadAndAdditionalInfoTest() {
         super();
     }
 
     @BeforeMethod()
-    public void start() throws InterruptedException {
+    public void start() throws Exception {
         initialization();
         Loginpage = new LoginPage();
         dup = new DocumentUploadPage();
         aip = new AdditionalInfoPage();
+        ClaimHm = new ClaimsHomePG();
+        Med = new Medical();
+        mis = new MIS();
+        Loginpage.NinjaLogin(prop.getProperty("ninjaemail"), prop.getProperty("ninjapass"));
+        Loginpage.ninja_MIS();
+        mis.MISPolicyNumber();
+        misPolicy = mis.policyNumber();
+        System.out.println(misPolicy);
+        mis.BackTo_Claims();
+
     }
 
     @Test
     public void AdditionalInfoPage() throws Exception {
-        Loginpage.ninja_Claims();
+        ClaimHm.SetupIncidentDetailPage(misPolicy);
+        Med.MedicalAsitancePageNavigation();  // For TC1  Assertion Point
+        Med.MoveForwardTest();  // For Tc2 Assertion Point
+        Med.VerifyWorkShopButton(); // For Tc3 Assertion Point
+
         dup.Doc();
         aip.SurveyorDetails();
     }
