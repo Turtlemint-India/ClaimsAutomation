eeae5057c518610bd2ee8c5ddfcc3ad44ffcf4cf
tejasbahadure <tejas.bahadure@turtlemint.com>
2023-03-09 14:17:09 +0530

    Code updated

--- src/main/java/com/qa/turtlemint/pages/MedWorkINIT/ClaimsHomePG.java
+++ src/main/java/com/qa/turtlemint/pages/MedWorkINIT/ClaimsHomePG.java
@@ -169,10 +169,9 @@ public class ClaimsHomePG extends TestBase
         TestUtil.waitUntilVisibilityOfElement(PolicyNo);
         TestUtil.sendKeys(PolicyNo,PolicyNumber,"Policy Number is entered");
       //  Thread.sleep(1500);
-      //  TestUtil.click(SaveDraft, "Save Draft clicked");
+        TestUtil.click(SaveDraft, "Save Draft clicked");
         TestUtil.getScreenShot();
         Thread.sleep(3000);
-        FetchDetail.click();
         TestUtil.click(FetchDetail,"detail fetch");
         WebCommands.staticSleep(500);
         TestUtil.getFullPageScreenShot();
